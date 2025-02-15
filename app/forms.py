39a8fa3e375afd78b9fb4081ce08f49efed16cc3
agrdev85 <afrometa674@gmail.com>
2025-02-15 10:09:37 -0500

    DB modified

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, Regexp
 
 
@@ -9,9 +9,10 @@ class LoginForm(FlaskForm):
 	submit = SubmitField("Acceder")
 
 class RegisterForm(FlaskForm):
-	name = StringField("Nombre:", validators=[DataRequired(), Length(max=50)])
-	phone = StringField("Movil:", validators=[DataRequired(), Length(max=30)])
-	email = StringField("Email:", validators=[DataRequired(), Email()])
-	password = PasswordField("Contraseña:", validators=[DataRequired(), Regexp("^[a-zA-Z0-9_\-&$@#!%^*+.]{8,30}$", message='La contraseña debe tener 8 caracteres y debe contener letras, números y símbolos.')])
-	confirm = PasswordField("Confirma la Contraseña:",validators=[EqualTo('password', message='Las contraseñas deben coincidir')])
-	submit = SubmitField("Registro")
+    name = StringField("Nombre:", validators=[DataRequired(), Length(max=50)])
+    phone = StringField("Movil:", validators=[DataRequired(), Length(max=30)])
+    membership = SelectField("Membresía:", choices=[('trial', 'Prueba'), ('basic', 'Básica'), ('premium', 'Profesional')], validators=[DataRequired()])
+    email = StringField("Email:", validators=[DataRequired(), Email()])
+    password = PasswordField("Contraseña:", validators=[DataRequired(), Regexp("^[a-zA-Z0-9_\-&$@#!%^*+.]{8,30}$", message='La contraseña debe tener 8 caracteres y debe contener letras, números y símbolos.')])
+    confirm = PasswordField("Confirma la Contraseña:", validators=[EqualTo('password', message='Las contraseñas deben coincidir')])
+    submit = SubmitField("Registro")
